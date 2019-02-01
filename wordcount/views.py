3b598a2728fd42b2fa45e4465fdc2348597f496b
mahithakur <thakurmahipal1@gmail.com>
2019-02-01 11:47:55 +0530

    first commit

---
+++ wordcount/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+def about(request):
+    return render(request, 'about.html')
+def homepage(request):
+    return render(request, 'home.html')
+def count(request):
+    data = request.GET['fulltextarea']
+    word_list = data.split()
+    worddictionary = {}
+
+    for word in word_list:
+        if word in worddictionary:
+            worddictionary[word] += 1
+        else:
+            worddictionary[word] = 1
+    list_length = len(word_list)
+   #sorted_list =  sorted( worddictionary.items(), key = operator.itemgetter(1), reverse= True)
+    return render(request, 'count.html' , {'fulltext': data , 'words': list_length , 'worddictionary':  worddictionary.items()})
